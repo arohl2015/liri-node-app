ff74a4c5bb165efa97278e38344fa6cf88b8e6d7
A Rohl <april.rohl2015@gmail.com>
2020-04-03 22:27:19 -0700

    creation of all variables/pseudocode

--- liri.js
+++ liri.js
@@ -1,5 +1,30 @@
+// Info for Spotify
 require("dotenv").config();
-
 var keys = require("./keys.js");
-
 var spotify = new Spotify(keys.spotify);
+var Spotify = require("node-spotify-api");
+
+// Core node package for reading and writing files
+var fs = require("fs");
+// Create var for axios to pull OMDB API and Bands in Town API data
+var axios = require("axios");
+//Rest of variables
+var moment = require("moment");
+var request = require("request");
+// Takes in all of the command line arguments listed below
+var command = process.argv[2];
+var search = process.argv[3];
+
+// //Make it so liri.js can take in one of the following commands:
+// concert-this
+// spotify-this-song
+// movie-this
+// do-what-it-says
+
+if (command === "concert-this") {
+  var bitURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+} else {
+    
+}
+
+
