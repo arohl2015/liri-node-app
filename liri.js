231a26d92effff79ace8f193c4aef398addfb268
A Rohl <april.rohl2015@gmail.com>
2020-04-04 22:22:17 -0700

    pseudocode added/start of var and functions

--- liri.js
+++ liri.js
@@ -13,18 +13,78 @@ var moment = require("moment");
 var request = require("request");
 // Takes in all of the command line arguments listed below
 var command = process.argv[2];
-var search = process.argv[3];
 
-// //Make it so liri.js can take in one of the following commands:
+//Make it so liri.js can take in each of the following commands:
+
 // concert-this
-// spotify-this-song
-// movie-this
-// do-what-it-says
+// render the following information about each event to the terminal:
+// Name of the venue
+// Venue location
+// Date of the Event (use moment to format this as "MM/DD/YYYY")
 
 if (command === "concert-this") {
-  var bitURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
-} else {
-    
+    var artist = process.argv[3];
+    var bandURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+    axios.get(bandURL).then(
+        function(response) {
+          console.log ("Venue: ");
+          console.log ("Location: ")
+          console.log ("Date: ")
+        }
+    )};
+  
+// spotify-this-song
+// show the following information about the song in your terminal/bash window
+// Artist(s)
+// The song's name
+// A preview link of the song from Spotify
+// The album that the song is from
+// If no song is provided then your program will default to "The Sign" by Ace of Base.
+ else if (command === "spotify-this-song") {
+
 }
+
+
+// movie-this
+// output the following information to your terminal/bash window:
+//   * Title of the movie.
+//   * Year the movie came out.
+//   * IMDB Rating of the movie.
+//   * Rotten Tomatoes Rating of the movie.
+//   * Country where the movie was produced.
+//   * Language of the movie.
+//   * Plot of the movie.
+//   * Actors in the movie.
+else if () {
+    if (search === undefined) {
+        console.log ()
+    }
+}
+
+else if () {
+    "http://www.omdbapi.com/?t=" + process.argv[3] + "&y=&plot=short&apikey=trilogy";
+  
+  }
+// do-what-it-says
+// Using the fs Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
+// It should run spotify-this-song for "I Want it That Way," as follows the text in random.txt.
+// Edit the text in random.txt to test out the feature for movie-this and concert-this.
+fs.readFile("random.txt", "utf8", function(err, data) {
+    if (err) {
+        return console.log(err);
+      }
+
+  
+  
+  
+  
+
+  
+  
+ 
+
+
+    
+
 
 
